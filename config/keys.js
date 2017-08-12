8f3570c99200898a441e57f39cd4a3d09f94c3be
Morgan Pierson <morganpierson@gmail.com>
2017-08-12 11:50:26 -0700

    clientid and secret

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  googleClientID:
+    "570907637120-f5bi8784kmhnb81e2d8qnj27kb17e620.apps.googleusercontent.com",
+  googleClientSecret: "wqd1cd33R6fDA8cCQY7deixt"
+};
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const app = express();
 
+passport.use(new GoogleStrategy());
+
 const PORT = process.env.PORT || 3000; //process.env.PORT represents an environment variable to later be determined by Heroku, otherwise use 3000 while in development
 app.listen(PORT, () => {
   console.log(`Listening on 3000`);
