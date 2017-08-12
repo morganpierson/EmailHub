73f3002c7f22a05b93b4346355310e2459057f53
Morgan Pierson <morganpierson@gmail.com>
2017-08-12 12:07:09 -0700

    using passport strategy with keys

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-
+const keys = require("./config/keys");
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 
 const PORT = process.env.PORT || 3000; //process.env.PORT represents an environment variable to later be determined by Heroku, otherwise use 3000 while in development
 app.listen(PORT, () => {
