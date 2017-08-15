af391ab1265cd5a54b0aa565ae80819db24671f0
Morgan Pierson <morganpierson@gmail.com>
2017-08-15 16:19:40 -0700

    cookie session

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
 
 const keys = require("./config/keys");
 const authRoutes = require("./routes/authRoutes");
@@ -10,6 +12,16 @@ require("./services/passport");
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 authRoutes(app);
 
